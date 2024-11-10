458c764490cb2ff47ef8cca15eadb87ae5dee8f1
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-09 18:09:12 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -272,7 +272,7 @@ export const managers = [
       "photo": "/managers/IIV.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2023, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "min", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "Ruibild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Rebuild", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "PrimeTime JG", // Can be anything (usually your rival's name)
         link: 8, // manager array number within this array, or null to link back to all managers page
