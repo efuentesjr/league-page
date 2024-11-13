840f891fa1cd6db09bb9edb5446bfc3dd40aa939
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-13 16:10:35 -0600

    Update leagueRecords.js

--- src/lib/utils/helperFunctions/leagueRecords.js
+++ src/lib/utils/helperFunctions/leagueRecords.js
@@ -465,8 +465,6 @@ const digestBracket = ({bracket, playoffRecords, playoffRounds, matchupDifferent
 		const {sPR, mD, pSD} =  processMatchups({matchupWeek, seasonPointsRecord, record: playoffRecords, startWeek, matchupDifferentials, year})
 
 		postSeasonData = meshPostSeasonData(postSeasonData, pSD);
-
-		postSeasonData = meshPostSeasonData(postSeasonData, pSD);
 		seasonPointsRecord = sPR;
 		matchupDifferentials = mD;
 	}
