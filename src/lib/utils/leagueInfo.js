61ccbd6224ee1a3266d7cc2b10ca4c3c355d0d2c
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-18 23:25:32 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -158,7 +158,7 @@ export const managers = [
       "name": "Gerardo Calles",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Austin TX", // (optional)
-      "bio": "Lorem ipsum...",
+      "bio": "The People's 2X Champ is ready for any and all challengers.",
       "photo": "/managers/GC.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2015, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "pit", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
@@ -171,7 +171,7 @@ export const managers = [
       "favoritePlayer": 7564, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
       "valuePosition": "WR", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
-      "philosophy": "hi",
+      "philosophy": "Be like water!",
       "tradingScale": 3, // 1 - 10
       "preferredContact": "Sleeper" // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
