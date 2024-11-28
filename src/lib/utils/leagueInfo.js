b3842f4c393670979f03c6d90116ae23c89d56ae
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2024-11-28 13:25:10 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -287,7 +287,7 @@ export const managers = [
       "preferredContact": "Sleeper", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
   	{
-      "roster": 13,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+      "roster": 14,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "name": "Nick Bowens",
       "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Austin TX", // (optional)
