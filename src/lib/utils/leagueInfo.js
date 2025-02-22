a3d1deed69a6ea37a5588fa2fabda67f83702798
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-02-22 16:36:21 -0600

    2025 Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1062784235112607744"; // your league ID
+export const leagueID = "1199099864982507520"; // your league ID
 export const leagueName = "MFFL"; // your league name
 export const dues = 75; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
