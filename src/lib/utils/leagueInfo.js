93128837abaa0ec2c6511255a74d2c1330a19c8d
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-03-03 18:21:24 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -260,7 +260,7 @@ export const managers = [
       "location": "Austin TX", // (optional)
       "bio": "If we playin, we STAY IN",
       "photo": "/managers/JG.jpg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2018 2021, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": [2018, 2021], // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "hou", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
