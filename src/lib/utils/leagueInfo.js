942a9c28f586d0098c6dd40b36996c3c90ac7c6d
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-03-12 22:49:30 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,8 +7,8 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = `
-  <img src="/managers/Picture1.png" alt="League Logo" style="width: 100%; height: auto; margin-top: 0px;" />
-  <p style="font-size: 13px;">Congratulations to Nick Bowens – Cee Dees TDs, the 2024 MFFL Champion! 🏆 The offseason is here, and trading is now open. Time to start planning for next season!!</p>
+<img src="/managers/Picture1.png" alt="League Logo" style="width: 100%; height: 100%; object-fit: fill; margin-top: 0px;" />
+  <p style="font-size: 18px;">Congratulations to Nick Bowens – Cee Dees TDs, the 2024 MFFL Champion! 🏆 The offseason is here, and trading is now open. Time to start planning for next season!!</p>
 `;
 
 /*   STEP 3   */
