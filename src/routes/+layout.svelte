da5feaf458b1cbed09011d593d24051add5e06f4
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-08-15 16:38:35 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,18 @@
-<script>export let data;</script>
+<script>
+  export let data;
+  import { onMount } from 'svelte';
+  let NavComp = null;
+</script>
+
 <slot />
+
+{#if NavComp}
+  <svelte:component this={NavComp} />
+{/if}
+
+<script>
+  onMount(async () => {
+    // Make sure the path & filename casing match your repo exactly
+    NavComp = (await import('$lib/components/Nav.svelte')).default;
+  });
+</script>
