807b6488995c14a5b6b692dc0fbd023c7d918e1b
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-08-17 15:19:16 -0500

    Update +page.svelte

--- src/routes/playoffs-projection/+page.svelte
+++ src/routes/playoffs-projection/+page.svelte
@@ -52,23 +52,23 @@
         </tr>
       </thead>
       <tbody>
-        <tr><td>N</td><td><a href="#">Brute Force</a></td><td>0-0-0</td><td>0</td><td>C:24.7%</td><td>C:41.8% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
-        <tr><td>N</td><td><a href="#">Comeback Kid</a></td><td>0-0-0</td><td>0</td><td>C:24.8%</td><td>C:41.7% T:17.4%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
-        <tr><td>E</td><td><a href="#">Muad'Dib 🎗</a></td><td>0-0-0</td><td>0</td><td>C:24.8%</td><td>C:41.7% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>S</td><td><a href="#">SlickBears 🎗</a></td><td>0-0-0</td><td>0</td><td>C:25.2%</td><td>C:42.1% T:17.0%</td><td>5</td><td>8.0-8.0</td><td>9</td><td>9.0-11.0</td></tr>
-        <tr><td>W</td><td><a href="#">People's Champ 🎗</a></td><td>0-0-0</td><td>0</td><td>C:25.1%</td><td>C:42.1% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
-        <tr><td>E</td><td><a href="#">Pete Weber Bowl Clu</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:41.8% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>E</td><td><a href="#">Fields love irving</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:42.0% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
-        <tr><td>S</td><td><a href="#">bLuE BaLLeRs</a></td><td>0-0-0</td><td>0</td><td>C:24.9%</td><td>C:41.9% T:17.3%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>W</td><td><a href="#">88boyz11</a></td><td>0-0-0</td><td>0</td><td>C:25.1%</td><td>C:42.1% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>W</td><td><a href="#">PrimeTime Prodigies</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:42.0% T:17.0%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>S</td><td><a href="#">TexasTimeshifts</a></td><td>0-0-0</td><td>0</td><td>C:24.9%</td><td>C:42.0% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>S</td><td><a href="#">Loud and Stroud</a></td><td>0-0-0</td><td>0</td><td>C:24.8%</td><td>C:41.7% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
-        <tr><td>N</td><td><a href="#">Bay Area Party</a></td><td>0-0-0</td><td>0</td><td>C:25.3%</td><td>C:42.1% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>E</td><td><a href="#">Vick2times</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:42.2% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
-        <tr><td>N</td><td><a href="#">CeeDees TDs 🏆</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:41.9% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-        <tr><td>W</td><td><a href="#">Do it to them</a></td><td>0-0-0</td><td>0</td><td>C:24.6%</td><td>C:41.7% T:17.4%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
-      </tbody>
+          <tr><td>N</td><td><a href="/team/brute-force">Brute Force</a></td><td>0-0-0</td><td>0</td><td>C:24.7%</td><td>C:41.8% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
+          <tr><td>N</td><td><a href="/team/comeback-kid">Comeback Kid</a></td><td>0-0-0</td><td>0</td><td>C:24.8%</td><td>C:41.7% T:17.4%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
+          <tr><td>E</td><td><a href="/team/muaddib">Muad'Dib 🎗</a></td><td>0-0-0</td><td>0</td><td>C:24.8%</td><td>C:41.7% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>S</td><td><a href="/team/slickbears">SlickBears 🎗</a></td><td>0-0-0</td><td>0</td><td>C:25.2%</td><td>C:42.1% T:17.0%</td><td>5</td><td>8.0-8.0</td><td>9</td><td>9.0-11.0</td></tr>
+          <tr><td>W</td><td><a href="/team/peoples-champ">People's Champ 🎗</a></td><td>0-0-0</td><td>0</td><td>C:25.1%</td><td>C:42.1% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
+          <tr><td>E</td><td><a href="/team/pete-weber-bowl-clu">Pete Weber Bowl Clu</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:41.8% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>E</td><td><a href="/team/fields-love-irving">Fields love irving</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:42.0% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
+          <tr><td>S</td><td><a href="/team/blue-ballers">bLuE BaLLeRs</a></td><td>0-0-0</td><td>0</td><td>C:24.9%</td><td>C:41.9% T:17.3%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>W</td><td><a href="/team/88boyz11">88boyz11</a></td><td>0-0-0</td><td>0</td><td>C:25.1%</td><td>C:42.1% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>W</td><td><a href="/team/primetime-prodigies">PrimeTime Prodigies</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:42.0% T:17.0%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>S</td><td><a href="/team/texastimeshifts">TexasTimeshifts</a></td><td>0-0-0</td><td>0</td><td>C:24.9%</td><td>C:42.0% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>S</td><td><a href="/team/loud-and-stroud">Loud and Stroud</a></td><td>0-0-0</td><td>0</td><td>C:24.8%</td><td>C:41.7% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
+          <tr><td>N</td><td><a href="/team/bay-area-party">Bay Area Party</a></td><td>0-0-0</td><td>0</td><td>C:25.3%</td><td>C:42.1% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>E</td><td><a href="/team/vick2times">Vick2times</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:42.2% T:17.1%</td><td>5</td><td>8.0-8.0</td><td>10</td><td>9.0-11.0</td></tr>
+          <tr><td>N</td><td><a href="/team/ceedees-tds">CeeDees TDs 🏆</a></td><td>0-0-0</td><td>0</td><td>C:25.0%</td><td>C:41.9% T:17.2%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+          <tr><td>W</td><td><a href="/team/do-it-to-them">Do it to them</a></td><td>0-0-0</td><td>0</td><td>C:24.6%</td><td>C:41.7% T:17.4%</td><td>5</td><td>8.0-8.0</td><td>11</td><td>9.0-11.0</td></tr>
+    </tbody>
     </table>
 
     <!-- Legend directly below table, INSIDE overlay -->
