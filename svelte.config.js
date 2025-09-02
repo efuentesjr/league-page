59c29a30d1cd4053cf4d2b8933aee9b077a275e8
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-09-02 15:07:12 -0500

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
 import vercel from '@sveltejs/adapter-vercel';
 import node from '@sveltejs/adapter-node';
 
-const dockerBuild = process.env.DOCKER_BUILD;
+const dockerBuild = process.env.DOCKER_BUILD
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: dockerBuild
-			? node()
-			: vercel({
-					runtime: 'nodejs22.x' // ✅ Explicitly tell Vercel to use Node 22
-			  })
+		adapter: dockerBuild ? node() : vercel(),
 	}
 };
 
