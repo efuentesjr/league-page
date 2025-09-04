b1f58343176a7db614890fc447f92324d04c2679
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-09-03 22:28:11 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <!-- __layout.svelte -->
 <script>
-  import { Nav, Footer } from '$lib/components';
+	import { Nav, Footer } from "$lib/components"
+    import { dev } from '$app/environment';
+    import { injectAnalytics } from '@vercel/analytics/sveltekit';
+ 
+    injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
-  <Nav /> <!-- adds the nav (small and large) -->
+    <Nav /> <!-- adds the nav (small and large) -->
   
-  <slot />
+    <slot />
 
-  <Footer /> <!-- adds the footer -->
+    <Footer /> <!-- adds the footer -->
 </main>
