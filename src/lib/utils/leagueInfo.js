3d2dfdb25581e3aa945a9a25e492e3933ba65a5d
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-09-15 22:37:02 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,8 +27,6 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 export const managers = [
   {
     "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
-    "managerID": "844760551790858240",           // <-- add Sleeper user_id
-    "slug": "brute-force-attack",                // <-- add site/team slug
     "name": "Eddie Fuentes Jr |Commish",
     "location": "Manor TX", // (optional)
     "bio": "Da Bears! Bears Down! 🐻⬇️",
