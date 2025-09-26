12896b1916ab886a3a76b82dcc9de9d90009457b
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-09-26 12:36:25 -0500

    Update DraftRow.svelte

--- src/lib/Drafts/DraftRow.svelte
+++ src/lib/Drafts/DraftRow.svelte
@@ -106,7 +106,7 @@
   /* === First-round mock image thumbnail === */
   .mockDraftImg {
     position: absolute;
-    left: 70%;
+    left: 60%;
     top: 40%;
     transform: translate(-50%, -50%);
     width: 70%;           /* tweak 50–75% as you like */
