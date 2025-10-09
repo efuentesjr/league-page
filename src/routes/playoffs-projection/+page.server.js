8d6a7bde51fb6fdf726a397d7f3152be1a74d465
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-10-09 15:27:28 -0500

    Update +page.server.js

--- src/routes/playoffs-projection/+page.server.js
+++ src/routes/playoffs-projection/+page.server.js
@@ -7,7 +7,7 @@ const slugMap = {
   'Bay Area Par': 'bay-area-party-supplies',
   'CeeDees TDs': 'ceedees-tds',          // fixed case/typo
   'Chosen one.': 'chosen-one',
-  'The People’s': 'peoples-champ',
+  'The Peoples': 'peoples-champ',
   'Pete Weber B': 'pete-weber-bowl-club',
   'Los Loquitos': 'los-loquitos',
   'bLuE BaLLeRs': 'blue-ballers',
