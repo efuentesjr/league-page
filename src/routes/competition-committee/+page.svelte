63ce9f06a0787ef89d6011c1ef8e106d70863ce1
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2025-11-20 09:08:24 -0600

    Update +page.svelte

--- src/routes/competition-committee/+page.svelte
+++ src/routes/competition-committee/+page.svelte
@@ -43,7 +43,7 @@
   }
 
   const proposals: Proposal[] = [
-    { id: 1, title: 'Offer incentives for Divisional Champs. OPTION#1: $25 payout, would require Fee increase. ', owner: 'Jordan Fristoe / Nick Bowens', status: 'OPEN' },
+    { id: 1, title: 'Offer incentives for Divisional Champs. OPTION#1: $25 payout (would require league Fee increase). OPTION#2: A bye week for the 1st round of playoffs. (Would require begining playoffs WK13 or end WK18). ', owner: 'Jordan Fristoe / Nick Bowens', status: 'NEW' },
     { id: 2, title: 'Review bookkeeping (dues, winnings, etc.)', owner: 'Commish', notes: 'Section 7.2, 7.3', status: 'OPEN' },
     { id: 3, title: 'Raise dues ($75 → $85); 2/3 vote needed to raise fees, 11 owners. Commish to provide rationale.', owner: 'Commish', notes: 'Section 7.3', status: 'OPEN' },
     { id: 4, title: 'Draw for 2026 draft order', owner: 'League', status: 'OPEN' },
