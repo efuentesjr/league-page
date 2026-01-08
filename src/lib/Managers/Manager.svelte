81d1e816d18d9c1cf175b28b922323c86f15cbd2
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2026-01-07 19:06:21 -0600

    Refactor Manager.svelte for improved readability

--- src/lib/Managers/Manager.svelte
+++ src/lib/Managers/Manager.svelte
@@ -1,14 +1,15 @@
+<!-- src/lib/Managers/Manager.svelte -->
 <script>
     import Button, { Group, Label } from '@smui/button';
-	import LinearProgress from '@smui/linear-progress';
-    import {loadPlayers, getLeagueTransactions} from '$lib/utils/helper';
-	import Roster from '../Rosters/Roster.svelte';
-	import TransactionsPage from '../Transactions/TransactionsPage.svelte';
+    import LinearProgress from '@smui/linear-progress';
+    import { loadPlayers, getLeagueTransactions } from '$lib/utils/helper';
+    import Roster from '../Rosters/Roster.svelte';
+    import TransactionsPage from '../Transactions/TransactionsPage.svelte';
     import { goto } from '$app/navigation';
     import ManagerFantasyInfo from './ManagerFantasyInfo.svelte';
     import ManagerAwards from './ManagerAwards.svelte';
     import { onMount } from 'svelte';
-	import { getDatesActive, getRosterIDFromManagerID, getTeamNameFromTeamManagers } from '$lib/utils/helperFunctions/universalFunctions';
+    import { getDatesActive, getRosterIDFromManagerID, getTeamNameFromTeamManagers } from '$lib/utils/helperFunctions/universalFunctions';
 
     export let manager, managers, rostersData, leagueTeamManagers, rosterPositions, transactionsData, awards, records;
 
@@ -18,18 +19,24 @@
 
     $: datesActive = getDatesActive(leagueTeamManagers, viewManager.managerID);
 
-    const  startersAndReserve = rostersData.startersAndReserve;
+    const startersAndReserve = rostersData.startersAndReserve;
     let rosters = rostersData.rosters;
 
-    $: ({rosterID, year} = viewManager.managerID ? getRosterIDFromManagerID(leagueTeamManagers, viewManager.managerID) : {rosterID: viewManager.roster, year: null});
+    $: ({ rosterID, year } = viewManager.managerID
+        ? getRosterIDFromManagerID(leagueTeamManagers, viewManager.managerID)
+        : { rosterID: viewManager.roster, year: null });
 
-    $: teamTransactions = transactions.filter(t => t.rosters.includes(parseInt(rosterID)));
+    $: teamTransactions = transactions.filter((t) => t.rosters.includes(parseInt(rosterID)));
 
     $: roster = rosters[rosterID];
 
-    $: coOwners = year && rosterID ? leagueTeamManagers.teamManagersMap[year][rosterID].managers.length > 1 : roster.co_owners;
+    $: coOwners = year && rosterID
+        ? leagueTeamManagers.teamManagersMap[year][rosterID].managers.length > 1
+        : roster.co_owners;
 
-    $: commissioner = viewManager.managerID ? leagueTeamManagers.users[viewManager.managerID].is_owner : false;
+    $: commissioner = viewManager.managerID
+        ? leagueTeamManagers.users[viewManager.managerID].is_owner
+        : false;
 
     let players, playersInfo;
     let loading = true;
@@ -37,10 +44,10 @@
     const refreshTransactions = async () => {
         const newTransactions = await getLeagueTransactions(false, true);
         transactions = newTransactions.transactions;
-    }
+    };
 
     onMount(async () => {
-        if(transactionsData.stale) {
+        if (transactionsData.stale) {
             refreshTransactions();
         }
         const playerData = await loadPlayers(null);
@@ -48,20 +55,20 @@
         players = playerData.players;
         loading = false;
 
-        if(playerData.stale) {
+        if (playerData.stale) {
             const newPlayerData = await loadPlayers(null, true);
             playersInfo = newPlayerData;
             players = newPlayerData.players;
         }
-    })
+    });
 
     const changeManager = (newManager, noscroll = false) => {
-        if(!newManager) {
+        if (!newManager) {
             goto(`/managers`);
         }
         manager = newManager;
-        goto(`/manager?manager=${newManager}`, {noscroll});
-    }
+        goto(`/manager?manager=${newManager}`, { noscroll });
+    };
 </script>
 
 <style>
@@ -191,8 +198,7 @@
         }
     }
 
-	@media (max-width: 450px) {
-
+    @media (max-width: 450px) {
         .basicInfo {
             height: 20px;
         }
@@ -204,10 +210,9 @@
         .infoTeam {
             height: 30px;
         }
-	}
+    }
 
     @media (max-width: 370px) {
-
         .basicInfo {
             height: 18px;
         }
@@ -224,37 +229,58 @@
 
 <div class="managerContainer">
     <div class="managerConstrained">
-        <img class="managerPhoto" src="{viewManager.photo}" alt="manager"/>
+        <img class="managerPhoto" src="{viewManager.photo}" alt="manager" />
         <h2>
             {viewManager.name}
-            <div class="teamSub">{coOwners ? 'Co-' : ''}Manager of <i>{getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year)}</i></div>
+            <div class="teamSub">
+                {coOwners ? 'Co-' : ''}Manager of
+                <i>{getTeamNameFromTeamManagers(leagueTeamManagers, rosterID, year)}</i>
+            </div>
         </h2>
-        
+
         <div class="basicInfo">
             <span class="infoChild">{viewManager.location || 'Undisclosed Location'}</span>
-            {#if viewManager.managerID && datesActive.start}
+
+            {#if viewManager.fantasyStart || (viewManager.managerID && datesActive.start)}
                 <span class="seperator">|</span>
-                {#if datesActive.end}
-                    <span class="infoChild">In the league from '{datesActive.start.toString().substr(2)} to '{datesActive.end.toString().substr(2)}</span>
+
+                {#if viewManager.fantasyStart}
+                    <!-- Use manager config for "league since" for everyone -->
+                    <span class="infoChild">
+                        In the league since '{viewManager.fantasyStart.toString().substr(2)}
+                    </span>
+                {:else if datesActive.end}
+                    <!-- Fallback: show date range if they left -->
+                    <span class="infoChild">
+                        In the league from '{datesActive.start.toString().substr(2)} to '{datesActive.end.toString().substr(2)}
+                    </span>
                 {:else}
-                    <span class="infoChild">In the league since '{datesActive.start.toString().substr(2)}</span>
+                    <!-- Fallback: computed active start -->
+                    <span class="infoChild">
+                        In the league since '{datesActive.start.toString().substr(2)}
+                    </span>
                 {/if}
-            {:else if viewManager.fantasyStart}
-                <!-- fantasyStart is an optional field -->
-                <span class="seperator">|</span>
-                <span class="infoChild">Playing ff since '{viewManager.fantasyStart.toString().substr(2)}</span>
             {/if}
+
             {#if viewManager.preferredContact}
                 <!-- preferredContact is an optional field -->
                 <span class="seperator">|</span>
-                <span class="infoChild">{viewManager.preferredContact}<img class="infoChild infoContact" src="/{viewManager.preferredContact}.png" alt="favorite team"/></span>
+                <span class="infoChild">
+                    {viewManager.preferredContact}
+                    <img class="infoChild infoContact" src="/{viewManager.preferredContact}.png" alt="favorite team" />
+                </span>
             {/if}
-            <!-- <span class="infoChild">{viewManager.preferredContact}</span> -->
+
             {#if viewManager.favoriteTeam}
                 <!-- favoriteTeam is an optional field -->
                 <span class="seperator">|</span>
-                <img class="infoChild infoTeam" src="https://sleepercdn.com/images/team_logos/nfl/{viewManager.favoriteTeam}.png" alt="favorite team"/>
+                <img
+                    class="infoChild infoTeam"
+                    src="https://sleepercdn.com/images/team_logos/nfl/{viewManager.favoriteTeam}.png"
+                    alt="favorite team"
+                />
             {/if}
+
             {#if commissioner}
                 <span class="seperator">|</span>
                 <div class="infoChild commissionerBadge">
@@ -266,23 +292,43 @@
         <div class="managerNav upper">
             <Group variant="outlined">
                 {#if manager == 0}
-                    <Button disabled class="selectionButtons" onclick={() => changeManager(parseInt(manager) - 1, true)} variant="outlined">
+                    <Button
+                        disabled
+                        class="selectionButtons"
+                        onclick={() => changeManager(parseInt(manager) - 1, true)}
+                        variant="outlined"
+                    >
                         <Label>Previous Manager</Label>
                     </Button>
                 {:else}
-                    <Button class="selectionButtons" onclick={() => changeManager(parseInt(manager) - 1, true)} variant="outlined">
+                    <Button
+                        class="selectionButtons"
+                        onclick={() => changeManager(parseInt(manager) - 1, true)}
+                        variant="outlined"
+                    >
                         <Label>Previous Manager</Label>
                     </Button>
                 {/if}
+
                 <Button class="selectionButtons" onclick={() => goto('/managers')} variant="outlined">
                     <Label>All Managers</Label>
                 </Button>
+
                 {#if manager == managers.length - 1}
-                    <Button disabled class="selectionButtons" onclick={() => changeManager(parseInt(manager) + 1, true)} variant="outlined">
+                    <Button
+                        disabled
+                        class="selectionButtons"
+                        onclick={() => changeManager(parseInt(manager) + 1, true)}
+                        variant="outlined"
+                    >
                         <Label>Next Manager</Label>
                     </Button>
                 {:else}
-                    <Button class="selectionButtons" onclick={() => changeManager(parseInt(manager) + 1, true)} variant="outlined">
+                    <Button
+                        class="selectionButtons"
+                        onclick={() => changeManager(parseInt(manager) + 1, true)}
+                        variant="outlined"
+                    >
                         <Label>Next Manager</Label>
                     </Button>
                 {/if}
@@ -303,7 +349,14 @@
         <ManagerFantasyInfo {viewManager} {players} {changeManager} />
     {/if}
 
-    <ManagerAwards {leagueTeamManagers} tookOver={viewManager.tookOver} {awards} {records} {rosterID} managerID={viewManager.managerID} />
+    <ManagerAwards
+        {leagueTeamManagers}
+        tookOver={viewManager.tookOver}
+        {awards}
+        {records}
+        {rosterID}
+        managerID={viewManager.managerID}
+    />
 
     {#if loading}
         <!-- promise is pending -->
@@ -312,7 +365,15 @@
             <LinearProgress indeterminate />
         </div>
     {:else}
-        <Roster division="1" expanded={false} {rosterPositions} {roster} {leagueTeamManagers} {players} {startersAndReserve} />
+        <Roster
+            division="1"
+            expanded={false}
+            {rosterPositions}
+            {roster}
+            {leagueTeamManagers}
+            {players}
+            {startersAndReserve}
+        />
     {/if}
 
     <h3>Team Transactions</h3>
@@ -324,7 +385,15 @@
                 <LinearProgress indeterminate />
             </div>
         {:else}
-            <TransactionsPage {playersInfo} transactions={teamTransactions} {leagueTeamManagers} show='both' query='' page={0} perPage={5} />
+            <TransactionsPage
+                {playersInfo}
+                transactions={teamTransactions}
+                {leagueTeamManagers}
+                show="both"
+                query=""
+                page={0}
+                perPage={5}
+            />
         {/if}
     </div>
 
@@ -339,9 +408,11 @@
                     <Label>Previous Manager</Label>
                 </Button>
             {/if}
+
             <Button class="selectionButtons" onclick={() => goto('/managers')} variant="outlined">
                 <Label>All Managers</Label>
             </Button>
+
             {#if manager == managers.length - 1}
                 <Button disabled class="selectionButtons" onclick={() => changeManager(parseInt(manager) + 1)} variant="outlined">
                     <Label>Next Manager</Label>
@@ -353,5 +424,4 @@
             {/if}
         </Group>
     </div>
-
 </div>
