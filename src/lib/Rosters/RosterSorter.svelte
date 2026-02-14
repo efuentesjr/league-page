5a04bf013722319a971efaf5d9c64be05474c72a
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2026-02-14 16:21:36 -0600

    Center background position for division banners

--- src/lib/Rosters/RosterSorter.svelte
+++ src/lib/Rosters/RosterSorter.svelte
@@ -50,22 +50,22 @@
 
 	.banner-D-1 {
 		background-image: url("/division-1-banner.png");
-		background-position: left; 
+		background-position: center; 
 	}
 
 	.banner-D-2 {
 		background-image: url("/division-2-banner.png");
-		background-position: right; 
+		background-position: center; 
 	}
 
 	.banner-D-3 {
 		background-image: url("/division-3-banner.png");
-		background-position: left; 
+		background-position: center; 
 	}
 
 	.banner-D-4 {
 		background-image: url("/division-4-banner.png");
-		background-position: left; 
+		background-position: center; 
 	}
 
 	h2 {
