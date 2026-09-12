b761f41ddd454c3802a1f2ab2ea87171f6035624
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2026-09-12 10:11:57 -0500

    Add ScoreTicker component to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <!-- __layout.svelte -->
 <script>
   import { Nav, Footer } from "$lib/components";
+  import ScoreTicker from "$lib/ScoreTicker.svelte";
   import { dev } from '$app/environment';
   import { injectAnalytics } from '@vercel/analytics/sveltekit';
   import { onMount } from 'svelte';
@@ -20,7 +21,7 @@
 
 <main>
   <Nav />
+  <ScoreTicker />
   <slot />
   <Footer />
 </main>
-
