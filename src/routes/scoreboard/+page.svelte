d7efb1a31d14c7bee28d65b8a45151822b1f9d5b
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2026-09-16 14:58:37 -0500

    Update +page.svelte

--- src/routes/scoreboard/+page.svelte
+++ src/routes/scoreboard/+page.svelte
@@ -590,8 +590,8 @@
 		background:
 			linear-gradient(
 				135deg,
-				rgba(25, 31, 39, 0.88),
-				rgba(5, 8, 12, 0.94)
+				rgba(75, 80, 86, 0.88),
+				rgba(35, 40, 45, 0.94)
 			);
 
 		box-shadow:
