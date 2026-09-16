5a5a6c0dfc142941e4b7d5d8b9738336c9053d68
Eddie Fuentes <145874318+efuentesjr@users.noreply.github.com>
2026-09-16 12:09:40 -0500

    Adjust font size for team name on scoreboard

--- src/routes/scoreboard/+page.svelte
+++ src/routes/scoreboard/+page.svelte
@@ -708,7 +708,7 @@
 		width: 100%;
 		max-width: 90%;
 
-		font-size: clamp(22px, 2vw, 40px);
+		font-size: clamp(18px, 1.5vw, 36px);
 		font-weight: 900;
 		letter-spacing: 1px;
 		line-height: 1.15;
